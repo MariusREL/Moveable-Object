b0bc17bf54cef3edafbb53c5bb27b07cd859219e
Marius R <Mrel0425@gmail.com>
2025-07-20 10:25:52 +0200

    Add movable circle functionality with basic styling

---
+++ app.js
@@ -0,0 +1,20 @@
+const circle = document.querySelector("#circle");
+
+document.addEventListener("DOMContentLoaded", () => {
+  circle.style.position = "absolute";
+  circle.style.top = "0";
+  circle.style.left = "0";
+  circle.style.bottom = "0";
+  circle.style.right = "0";
+  circle.style.margin = "auto";
+});
+
+document.addEventListener("keydown", (e) => {
+  console.log(e.code);
+  if (e.code === "KeyW") {
+    circle.style.top = parseInt(circle.style.top) - 10 + "px";
+  }
+  if (e.code === "KeyS") {
+    circle.style.top = parseInt(circle.style.top) + 10 + "px";
+  }
+});
