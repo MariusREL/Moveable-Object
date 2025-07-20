d29ffc486355ddd393b9f2b44d068c86797e7c51
Marius R <Mrel0425@gmail.com>
2025-07-20 11:42:35 +0200

    Refactor circle centering logic and update CSS reset styles

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const circle = document.querySelector("#circle");
 
-document.addEventListener("DOMContentLoaded", () => {
+// gets the size of the window and the size/position of the circle div, and centers it by dividing the space by half to find the exact center
+function centerCircle() {
   circle.style.position = "absolute";
-  circle.style.top = "0";
-  circle.style.left = "0";
-  circle.style.bottom = "0";
-  circle.style.right = "0";
-  circle.style.margin = "auto";
-});
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const circlePos = circle.getBoundingClientRect();
+  circle.style.top = windowHeight / 2 - circlePos.height / 2 + "px";
+  circle.style.left = windowWidth / 2 - circlePos.width / 2 + "px";
+}
+
+// centers the circle on both page load and resize
+window.addEventListener("DOMContentLoaded", centerCircle);
+window.addEventListener("resize", centerCircle);
 
 document.addEventListener("keydown", (e) => {
   console.log(e.code);
-  if (e.code === "KeyW") {
+  if (e.code === "KeyW" && parseInt(circle.style.top) <= 800) {
     circle.style.top = parseInt(circle.style.top) - 10 + "px";
   }
   if (e.code === "KeyS") {
